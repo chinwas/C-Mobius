1e960f0fb0b8b0a4a25cbe054659638902230ebe
codeman123454321 <chinmayw1704@gmail.com>
2025-10-01 22:14:36 +0530

    Inital Commit

---
+++ jane1.cpp
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, vs?\n");
+    return 0;
+    
+}
---
+++ jane2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//#include <string.h>
+
+int i = (10);
+
+int main() {
+    while (i >=5) {
+    
+        printf("Hello, jane2?$\n");
+        i -= 1;
+
+    }
+
+    if (i <= 5) {
+        printf("i is less than 5\n");
+    }
+
+
+    printf("kya haal hai");
+    return 0;
+    
+}
